ae7012bd0b287a19a1d8437349b49f16ab5cb6af
christineeee <41602221+christineeee@users.noreply.github.com>
2018-07-25 10:30:34 +0800

    Create Cexam.js

---
+++ Cexam.js
@@ -0,0 +1,118 @@
+[
+  {
+    "id": 145,
+    "name": " Isla de Gigantes
+Carles, Iloilo",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/1.jpg",
+    "Distance(Km)": 100km,
+    "lat": 11.599976,
+    "lng": 123.339958
+  },
+  {
+    "id": 148,
+    "name": "Bohol",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/10.jpg",
+    "Distance(Km)": 1,242km,
+    "lat": 9.581823,
+    "lng": 123.878187
+  },
+  {
+    "id": 201,
+    "name": "The Dessert Musseum Pasay ",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/11.jpg",
+    "Distance(Km)": 288km,
+    "lat": 14.531194,
+    "lng": 120.980588
+  },
+  {
+    "id": 205,
+    "name": "Patapat Bridge Pagudpud",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/12.jpg",
+    "Distance(Km)": 293km,
+    "lat": 18.576695,
+    "lng": 120.893608
+  },
+  {
+    "id": 217,
+    "name": " Monasterio De Tarlac San Jose, Tarlac",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/13.PNG",
+    "Distance(Km)": 190km,
+    "lat": 15.437748,
+    "lng": 15.437748
+  },
+  {
+    "id": 280,
+    "name": "Palaui Island in Cagayan Valley, Philippines",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/14.png",
+    "Distance(Km)": 2km,
+    "lat": 18.555929,
+    "lng":  122.139985
+  },
+  {
+    "id": 910,
+    "name": "Samal Island
+Davao del Norte",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/2.jpg",
+    "Distance(Km)": 100km,
+    "lat": 7.102158,
+    "lng": 7.102158
+  },
+  {
+    "id": 910,
+    "name": "Southern Cebu (Kawasan Falls)",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/3.jpg",
+    "Distance(Km)": 1,171km,
+    "lat": 9.802616,
+    "lng": 123.374089
+  },
+  {
+    "id": 910,
+    "name": "Sagada Mountain Provice",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/4.jpg",
+    "Distance(Km)": 159km,
+    "lat": 17.098412,
+    "lng": 120.909152
+  },
+  {
+    "id": 910,
+    "name": "Lake Sebu",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/5.jpg",
+    "Distance(Km)": 200km,
+    "lat": 6.244378,
+    "lng": 124.553437
+  },
+  {
+    "id": 910,
+    "name": "Minalungao National Park
+General Tinio, Nueva Ecija",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/6.jpg",
+    "Distance(Km)": 232km,
+    "lat": 15.298439,
+    "lng": 121.122842
+  },
+  {
+    "id": 910,
+    "name": "Siquijor",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/7.jpg",
+    "Distance(Km)": 1,241km,
+    "lat": 9.207530,
+    "lng": 123.476597
+  },
+  {
+    "id": 910,
+    "name": "Sohoton Bay
+Bucas Grande Islands, Surigao del Norte",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/8.jpg",
+    "Distance(Km)": 100km,
+    "lat": 9.614599,
+    "lng":  125.896603
+  },
+  {
+    "id": 910,
+    "name": "Secret Lagoon(El Nido)",
+    "imageUrl": "https://raw.githubusercontent.com/christineeee/chrisA/master/New%20folder/9.jpg",
+    "Distance(Km)": 20km,
+    "lat": 11.139734,,
+    "lng": 11.139734,
+  }  
+]
